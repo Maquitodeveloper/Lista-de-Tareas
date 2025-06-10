9342a3c04b6624247d56633243600cddbe1ace99
Alexander Guanchez <christianpaypal09@gmail.com>
2025-06-10 15:29:02 -0400

    added
    styles
    .js

---
+++ script.js
@@ -0,0 +1,17 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") return;
+
+  const taskList = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <span>${taskText}</span>
+    <button onclick="this.parentElement.remove()">Eliminar</button>
+  `;
+
+  taskList.appendChild(li);
+  taskInput.value = "";
+}
